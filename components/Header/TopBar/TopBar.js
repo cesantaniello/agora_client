c37dba9f5456f357f004a5078feaffa7d70f3cdc
Administrator <admin@example.com>
2022-02-23 12:50:14 +0100

    Agrega Logo en TopBar

--- components/Header/TopBar/TopBar.js
+++ components/Header/TopBar/TopBar.js
@@ -1,5 +1,32 @@
+import {Container, Grid, GridColumn, Image, Input} from 'semantic-ui-react';
+import Link from 'next/link';
+
 export default function TopBar() {
   return (
-    <h1>TopBar</h1>
+    <div className="top-bar">
+      <Container>
+        <Grid className="top-bar">
+
+          <GridColumn width={8} className="top-bar__left">
+            <Logo/>
+          </GridColumn>
+
+          <GridColumn width={8} className="top-bar__right">
+            <h2>Buscador</h2>
+          </GridColumn>
+
+        </Grid>
+      </Container>
+    </div>
   )
+}
+
+function Logo(){
+  return (
+    <Link href="/">
+      <a>
+        <Image src="/logo.png" alt="Gaming"/>
+      </a>
+    </Link>
+  );
 }
