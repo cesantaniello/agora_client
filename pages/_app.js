97c5bd196b7da03aa4077efb809185af91b437e5
Administrator <admin@example.com>
2022-02-23 10:56:31 +0100

    Implementa Semantic UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../scss/global.scss";
+import 'semantic-ui-css/semantic.min.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import { Button, Icon } from 'semantic-ui-react'
+
 export default function Home() {
   return (
     <div className="home">
       <h1>Estamos en Next</h1>
+      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
+      <Icon name="home" />
     </div>
   )
 }
