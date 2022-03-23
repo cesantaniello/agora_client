c959130e5d197ebf7dd83eba771d7f2546f81603
Administrator <admin@example.com>
2022-03-23 12:25:11 +0100

    Obten juegos por plataformas

--- pages/games/[platform].js
+++ pages/games/[platform].js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {useRouter} from 'next/router';
 import BasicLayout from '../../layouts/BasicLayout';
+import { getGamesPlatformApi } from '../../api/game';
+
+const limitPerPage = 10;
 
 export default function Platform() {
   const {query} = useRouter();
+  const [games, setGames] = useState(null);
+
+  useEffect(() => {
+    (async() => {
+      const response = await getGamesPlatformApi(query.platform, limitPerPage, 0);
+      setGames(response);    
+    })();
+  }, [query]);
 
   return (
     <BasicLayout className="platform">
