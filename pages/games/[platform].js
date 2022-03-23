68d00706f9708cb082015eefd2c47aa4a68f195e
Administrator <admin@example.com>
2022-03-23 13:30:54 +0100

    Genera datos para paginacion

--- pages/games/[platform].js
+++ pages/games/[platform].js
@@ -3,21 +3,41 @@ import { Loader } from 'semantic-ui-react';
 import {useRouter} from 'next/router';
 import { size } from 'lodash';
 import BasicLayout from '../../layouts/BasicLayout';
-import { getGamesPlatformApi } from '../../api/game';
+import { getGamesPlatformApi, getTotalGamesPlatformApi } from '../../api/game';
 import ListGames from '../../components/ListGames';
 
-const limitPerPage = 10;
+const limitPerPage = 5;
 
 export default function Platform() {
   const {query} = useRouter();
   const [games, setGames] = useState(null);
+  const [totalGames, setTotalGames] = useState(null);
 
+  
+  const getStartItem = () => {
+    const currentPages = parseInt(query.page);
+    if (!query.page || currentPages === 1){
+      return 0;
+    } else {
+      return (currentPages - 1) * limitPerPage;
+    }
+  }
+
+  console.log(getStartItem());
+  
   useEffect(() => {
     (async() => {
       if (query.platform) {
         const response = await getGamesPlatformApi(query.platform, limitPerPage, 0);
         setGames(response);    
       }
+    })();
+  }, [query]);
+
+  useEffect(() => {
+    (async() => {
+      const response = await getTotalGamesPlatformApi(query.platform);
+      setTotalGames(response);
     })();
   }, [query]);
 
