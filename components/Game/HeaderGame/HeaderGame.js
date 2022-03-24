1310d0a6981684eeb81aeb6b1b4ed2dabcca375a
Administrator <admin@example.com>
2022-03-24 20:10:06 +0100

    Agrega informacion a card del juego

--- components/Game/HeaderGame/HeaderGame.js
+++ components/Game/HeaderGame/HeaderGame.js
@@ -21,7 +21,7 @@ export default function HeaderGame(props) {
 
 function Info(props){
   const { game } = props;
-  const { title, summary } = game;
+  const { title, summary, price, discount } = game;
 
   return (
     <>
@@ -29,10 +29,24 @@ function Info(props){
         {title}
         <Icon name='heart outline' link />
       </div>
-      <div 
-        className='header-game__summary' 
-        dangerouslySetInnerHTML={{__html: summary}}
+      <div className="header-game__delivery">Entrega en 24/48h</div>
+      <div
+        className="header-game__summary"
+        dangerouslySetInnerHTML={{ __html: summary }}
       />
+      <div className="header-game__buy">
+        <div className="header-game__buy-price">
+          <p>Precio de venta al público: {price} €</p>
+          <div className="header-game__buy-price-actions">
+            <p>-{discount}%</p>
+            <p> {(price - Math.floor(price * discount) / 100)} €</p>
+          </div>
+        </div>
+        <Button
+          className="header-game__buy-btn">
+          Comprar
+        </Button>
+      </div>
     </>
   )
 }
