2630babcf733229dcec60ebc29799e28df896cc7
Administrator <admin@example.com>
2022-03-24 16:49:29 +0100

    Obten datos de juego

--- pages/[game].js
+++ pages/[game].js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import BasicLayout from '../layouts/BasicLayout';
+import { getGameByUrlApi } from '../api/game';
 
 export default function Game() {
+  const [game, setGame] = useState(null);
   const {query} = useRouter();
+
+  useEffect(() => {
+    (async() => {
+      const response = await getGameByUrlApi(query.game);
+      setGame(response);
+    })();
+  }, [query]);
 
   return (
     <div>
