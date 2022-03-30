df0f1b058b1eef72474cffff8cb487a7c4486029
Administrator <admin@example.com>
2022-03-30 10:57:44 +0200

    Renderiza lista de juegos

--- pages/wishlist.js
+++ pages/wishlist.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import { Loader } from 'semantic-ui-react';
 import { size, forEach } from 'lodash';
 import BasicLayout from "../layouts/BasicLayout";
 import { getFavoriteApi } from '../api/favorite';
 import useAuth from '../hooks/useAuth';
+import ListGames from '../components/ListGames';
 
 export default function wishlist() {
   const [ games, setGames ] = useState(null);
@@ -11,7 +13,15 @@ export default function wishlist() {
   useEffect(() => {
     (async() => {
       const response = await getFavoriteApi(auth.idUser, logout);
-      setGames(response);
+      if(size(response)>0) {
+        const gameList = [];
+        forEach(response, (data) => {
+          gameList.push(data.game);
+        });
+        setGames(gameList);
+      } else {
+        setGames([]);
+      }
     })();
   },[]);
 
@@ -20,7 +30,13 @@ export default function wishlist() {
       <div className='wishlist__block'>
         <div className='title'>Lista de favoritos</div>
         <div className='data'>
-          <p>Lista de juegos</p>
+          {!games && <Loader active>Cargando juegos...</Loader>}
+          {games && size(games) === 0 && (
+            <div className="data__not-found">
+              <h3>No hay juegos en tu lista de favoritos</h3>
+            </div>
+          )}
+          {size(games) > 0 && <ListGames games={games} />}
         </div>
       </div>
     </BasicLayout>
