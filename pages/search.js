3ec1f75ba7fd054a3b184c0b62001f9a49f91ee7
Administrator <admin@example.com>
2022-03-30 11:12:57 +0200

    Crea pagina de busqueda

---
+++ pages/search.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BasicLayout from '../layouts/BasicLayout';
+
+export default function search() {
+  return (
+    <BasicLayout className="search">
+      <h1>Búsqueda...</h1>
+    </BasicLayout>
+  )
+}
