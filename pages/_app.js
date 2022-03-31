2f61ae01d453776e27b4011e32d1782cfd7b45f7
Administrator <admin@example.com>
2022-03-31 12:39:37 +0200

    Crea CartContext y useCart

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import {ToastContainer} from 'react-toastify';
 import jwtDecode from 'jwt-decode';
 import {useRouter} from 'next/router';
 import AuthContext from '../context/AuthContext';
+import CartContext from '../context/CartContext';
 import { setToken, getToken, removeToken } from '../api/token';
 import "../scss/global.scss";
 import 'semantic-ui-css/semantic.min.css';
@@ -51,20 +52,33 @@ export default function MyApp({ Component, pageProps }) {
     [auth]
   );
 
+  const cartData = useMemo(
+    () => ({
+      productsCart: 0,
+      addProductCart: () => null,
+      getProductsCart: () => null,
+      removeProductCart: () => null,
+      removeAllProductsCart: () => null,
+    }),
+    []
+  );
+
   return (
     <AuthContext.Provider value={authData}>
-      <Component {...pageProps} />
-      <ToastContainer 
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss={false}
-        draggable
-        pauseOnHover
-      />
+      <CartContext.Provider value={cartData}>
+        <Component {...pageProps} />
+        <ToastContainer 
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable
+          pauseOnHover
+          />
+      </CartContext.Provider>
     </AuthContext.Provider>
   );
 }
