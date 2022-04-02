4545ed5aafb81c670de5b5a7083d6f8accc24a54
Administrator <admin@example.com>
2022-04-02 15:19:09 +0200

    Crea pagina de carrito

---
+++ pages/cart.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import BasicLayout from '../layouts/BasicLayout'
+
+export default function Cart() {
+  return (
+    <BasicLayout className="cart">
+      <h1>cart</h1>
+    </BasicLayout>
+  )
+}
