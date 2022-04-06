41315fe6be10c622b7e553980b0554a650fad9bf
Administrator <admin@example.com>
2022-04-06 12:02:29 +0200

    Agrega dependencias de stripe

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,5 @@
 export const BASE_PATH = 'http://localhost:1337';
 export const TOKEN = "token";
 export const CART = "cart";
+export const STRIPE_TOKEN = 
+  "pk_test_51KlFKJJkDkH9iZQm2ZTErzpabFLjK3xOMRSNIxVjNh7g5uyQkvV760R6QwhMm5VWQzGQS2H89bawNPJeBderZWbA00dhXkQV4a";
