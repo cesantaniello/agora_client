e86fe10e7857baf157807d32dafda22dd0d47190
Administrator <admin@example.com>
2022-04-07 12:49:32 +0200

    Obten pedidos de usuario

--- pages/orders.js
+++ pages/orders.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Grid } from 'semantic-ui-react';
 import BasicLayout from '../layouts/BasicLayout';
+import { getOrdersApi } from '../api/order';
+import useAuth from '../hooks/useAuth';
 
 export default function Orders() {
+  const [orders, setOrders] = useState(null);
+  const { auth, logout } = useAuth();
+
+  useEffect(() => {
+    (async () => {
+      const response = await getOrdersApi(auth.idUser, logout);
+      setOrders(response || []);
+    })();
+  }, []);
+
   return (
     <BasicLayout className="orders">
       <div className="orders__block">
